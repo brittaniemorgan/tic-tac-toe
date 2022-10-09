665ec15fcef1f3913403424f4d00c56eb3fddf0f
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2022-10-08 23:59:08 -0500

    Added exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,36 @@
-window.onload = boot;
+window.onload = function(){
+    let tilesArr = document.querySelectorAll("#board div");
+    boot();
 
-function boot(){
-    console.log("boot");
-    document.querySelectorAll("#board div").forEach(el => el.className = "square");
+    //////////////// Part 1  
+        function boot(){
+            console.log("boot");
+            tilesArr.forEach(el => el.className = "square");
+        }
+
+    /////////Variables
+
+    tilesArr = tilesArr.forEach(tile => tile.addEventListener("click", fillSquare));
+    let player = "X";
+    let gameState = ["","","",
+                    "","","",
+                    "","","",];
+    /////////////// Part 2
+    function fillSquare(event){
+        const tile = event.target;
+        console.log(tile);
+        for (let i = 0; i<9; i++){ //make dependent on win
+            if (player == "X")
+            {
+                tile.innerHTML = "X";
+                tile.classList.add(".square.X");
+                player = "O";
+            }
+            else{
+                tile.innerHTML = "O";
+                tile.classList.add(".square.O");
+                player = "X";
+            }
+        }    
+    }
 }
